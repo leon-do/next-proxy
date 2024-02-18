91fd939df3a2c5006dce269430818463a6208b15
leon-do <leondo123@gmail.com>
2024-02-18 17:36:31 -0500

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/graphql",
+        destination: "https://api.uniswap.org/v1/graphql",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/graphql",
+        headers: [{ key: "Origin", value: "http://localhost:3000" }],
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
