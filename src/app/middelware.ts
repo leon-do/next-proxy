9187dc7a191599b40245e31c288344590875ad74
leon-do <leondo123@gmail.com>
2024-02-18 20:51:00 -0500

    middleware

---
+++ src/app/middelware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+
+export function middleware() {
+  // retrieve the current response
+  const res = NextResponse.next();
+
+  // add the CORS headers to the response
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT"
+  );
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  // add origin
+  res.headers.append("Origin", "http://localhost:3000");
+
+  return res;
+}
+
+// specify the path regex to apply the middleware to
+export const config = {
+  matcher: "/graphql",
+};
